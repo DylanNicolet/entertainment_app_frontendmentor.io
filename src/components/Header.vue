6fa2bb50aa68c6c2d1ae6d4f0d57d9b4ff0c7cd4
Dylan Nicolet <dylannicolet@hotmail.com>
2023-07-21 17:20:00 +0400

    [ADDED] Header with nav

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <img src="../assets/logo.svg" class="header__logo" alt="logo">
+        <nav>
+            <router-link to="/"><img src="../assets/icon-nav-home.svg" alt=""></router-link>
+            <router-link to="/about"><img src="../assets/icon-nav-movies.svg" alt=""></router-link>
+            <router-link to="/about"><img src="../assets/icon-nav-tv-series.svg" alt=""></router-link>
+            <router-link to="/about"><img src="../assets/icon-nav-bookmark.svg" alt=""></router-link>
+        </nav>
+        <section class="header__profile-button">
+            <router-link to="/"><img src="../assets/image-avatar.png" alt="Go to profile settings"></router-link>
+        </section>
+    </header>
+</template>
