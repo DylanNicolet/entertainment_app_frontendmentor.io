d0910e1958db7beccab0a271eab2964d466e7ff5
Dylan Nicolet <dylannicolet@hotmail.com>
2023-07-24 17:21:31 +0400

    [WORKED ON] Homepage

---
+++ src/components/Search.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="search">
+        <img class="search__icon" src="../assets/icon-search.svg" alt="search-icon">
+        <input class="search__input" type="text" placeholder="Search for movies or TV series">
+    </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,80 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <Search />
+
+        <section class="trending">
+            <h1 class="trending__title">Trending</h1>
+            <swiper-container :slides-per-view="screenWidth === 'mobile' ? 1.7 : 2.7">
+                <swiper-slide v-for="item in trendingData"  class="trending-card">
+                    <img :src="screenWidth === 'mobile' ? item.thumbnail.trending.small : item.thumbnail.trending.large" :alt="item.title" class="trending-card__image">
+                    <section class="trending-card__gradient"></section>
+                    <section class="trending-card__info">
+                        <p>{{item.year}}</p>
+                        &#x2022;
+                        <section class="info-category">
+                            <img :src="item.category === 'Movie' ? '../assets/icon-category-movie.svg' : '../assets/icon-category-tv.svg'" alt="">
+                            <p>{{item.category}}</p>
+                        </section>
+                        &#x2022;
+                        <p>{{ item.rating }}</p>
+                        <h2 class="info-title">{{ item.title }}</h2>
+                    </section>
+                </swiper-slide>
+            </swiper-container>
+        </section>
+    </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import Search from '@/components/Search.vue';
+import database from "../../data.json";
+import { register } from 'swiper/element/bundle';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 export default {
-  name: 'Home',
-  components: {
-    Header
-  }
+    name: 'Home',
+    components: {
+        Header,
+        Search
+    },
+
+    setup() {
+        let screenWidth = ref(undefined)
+
+        function handleScreenResize() {
+            let windowWidth = window.innerWidth
+
+            if ( windowWidth < 768 ) {
+                screenWidth.value = "mobile"
+                return false
+            } else if ( windowWidth >= 768 && windowWidth < 920 ) {
+                screenWidth.value = "tablet"
+                return false
+            } else if ( windowWidth >= 920 ) {
+                screenWidth.value = "desktop"
+            }
+        }
+
+        onMounted( () => {
+            window.addEventListener( 'resize', handleScreenResize )
+        } )
+
+        onUnmounted( () => {
+            window.removeEventListener( 'resize', handleScreenResize )
+        } )
+
+        //register Swiper
+        register()
+
+        //Generate data for trending section
+        let trendingData = database.filter( item => {
+            if ( item.thumbnail.trending ) {
+                return true
+            }
+        } )
+
+        return{trendingData, screenWidth}
+    }
 }
 </script>
