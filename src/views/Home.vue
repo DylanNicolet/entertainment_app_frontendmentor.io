e8ce4ab39edc6d100c65cd68634c2bc44980f556
Dylan Nicolet <dylannicolet@hotmail.com>
2023-07-28 17:23:30 +0400

    [WORKED ON] Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,6 +22,26 @@
                 </swiper-slide>
             </swiper-container>
         </section>
+
+        <section class="recommended">
+            <h1 class="recommended__title">Recommended for you</h1>
+            <section class="recommended__content">
+                <section v-for="item in recommendedData" class="recommended-card">
+                    <img :src="item.thumbnail.regular.small" :alt="item.title" class="recommended-card__image">
+                    <section class="recommended-card__info">
+                        <p>{{ item.year }}</p>
+                        &#x2022;
+                        <section class="info-category">
+                            <img :src="item.category === 'Movie' ? '../assets/icon-category-movie.svg' : '../assets/icon-category-tv.svg'" alt="">
+                            <p>{{ item.category }}</p>
+                        </section>
+                        &#x2022;
+                        <p>{{ item.rating }}</p>
+                        <h2 class="info-title">{{ item.title }}</h2>
+                    </section>
+                </section>
+            </section>
+        </section>
     </div>
 </template>
 
@@ -30,7 +50,7 @@ import Header from '@/components/Header.vue';
 import Search from '@/components/Search.vue';
 import database from "../../data.json";
 import { register } from 'swiper/element/bundle';
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted, watchEffect } from 'vue';
 
 export default {
     name: 'Home',
@@ -40,7 +60,7 @@ export default {
     },
 
     setup() {
-        let screenWidth = ref(undefined)
+        let screenWidth = ref( undefined )
 
         function handleScreenResize() {
             let windowWidth = window.innerWidth
@@ -74,7 +94,27 @@ export default {
             }
         } )
 
-        return{trendingData, screenWidth}
+        //Generate data for recommended section
+        let recommendedData = database.filter( item => {
+            if ( !item.thumbnail.trending ) {
+                return true
+            }
+        } )
+
+        //Create favoriteData locally
+        watchEffect( () => {
+            if ( !localStorage.getItem( 'favoriteData' ) ) {
+                localStorage.setItem( 'favoriteData', JSON.stringify( [] ) )
+            }
+        } )
+
+        function addToFavorite() {
+            let favoriteData = JSON.parse( localStorage.getItem( "favoriteData" ) || "[]" )
+
+            continue work here with isBookmarked from database <----------------
+        }
+
+        return{trendingData, recommendedData, screenWidth}
     }
 }
 </script>
